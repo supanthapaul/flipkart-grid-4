88c2dc6175af4bdc48c62dfc5eab233a0cc236ca
Supantha Paul <supapaul56@gmail.com>
2022-07-26 11:44:34 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const address = useAddress();
@@ -6,6 +7,7 @@ function App() {
   const disconnectWallet = useDisconnect();
   return (
     <div>
+			<Navbar />
       {address ? (
         <>
           <button onClick={disconnectWallet}>Disconnect Wallet</button>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import './index.css';
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Mumbai;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -17,7 +17,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
